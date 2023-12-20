24488f6124249b1b25dca749ecd447c7509eac6a
pragadeeswaran17 <pragadeeswaran647@gmail.com>
2023-12-20 14:51:14 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
+import React from "react";
 import Home from "./Routes/Home";
 import About from "./Routes/About";
 import Contact from "./Routes/Contact";
 import Project from "./Routes/Project";
-import { Route, Routes } from 'react-router-dom';
-import "./index.css"
-
+import { Route, Routes } from "react-router-dom";
+import "./index.css";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/project" element={<Project/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path="" element={<Home />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
-    
   );
 }
 
